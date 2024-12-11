dc47fa042808f5db9ee098a71704fd5c2e5080f1
kapoorisha221 <kapooresha8871@gmail.com>
2024-12-11 13:13:22 +0530

    adding teams

---
+++ Database2/Tables/FactInternetSales.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[FactInternetSales] (
+    [ProductKey]       INT           NOT NULL,
+    [OrderDateKey]     INT           NOT NULL,
+    [CustomerKey]      INT           NOT NULL,
+    [PromotionKey]     INT           NOT NULL,
+    [SalesOrderNumber] NVARCHAR (20) NOT NULL,
+    [OrderQuantity]    SMALLINT      NOT NULL,
+    [UnitPrice]        MONEY         NOT NULL,
+    [SalesAmount]      MONEY         NOT NULL
+)
+WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = HASH([ProductKey]));
+
